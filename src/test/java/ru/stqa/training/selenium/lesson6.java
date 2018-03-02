b98c298fe3e3926ad1a526de565cfb2e823126a5
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2018-03-02 14:52:42 +0300

    registration

--- src/test/java/ru/stqa/training/selenium/lesson6.java
+++ src/test/java/ru/stqa/training/selenium/lesson6.java
@@ -2,7 +2,6 @@ package ru.stqa.training.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -14,54 +13,52 @@ public class lesson6 extends TestBase{
   public void registration(){
    driver.get("http://localhost/litecart/en/");
    driver.findElement(By.xpath("//form[@name = 'login_form']//a")).click();
-    long random = Instant.now().toEpochMilli();
-    String index = String.valueOf(random).substring(8);
-    String phoneNumber = String.valueOf(random).substring(3);
+   long random = Instant.now().toEpochMilli();
+   String index = String.valueOf(random).substring(8);
+   String phoneNumber = String.valueOf(random).substring(3);
+   String email = random + "@mail.ru";
+   String pass = "password_" + random;
 
-    WebElement nameField = driver.findElement(By.xpath("//form[@name = 'customer_form']//input[@name = 'firstname']"));
-    WebElement countriesList = driver.findElement(By.xpath("//form[@name = 'customer_form']//select[@name = 'country_code']"));
-    WebElement zonesList = driver.findElement(By.xpath("//form[@name = 'customer_form']//select[@name = 'zone_code']"));
-    WebElement email = driver.findElement(By.xpath("//form[@name = 'customer_form']//input[@name = 'email']"));
     WebElement phone = driver.findElement(By.xpath("//form[@name = 'customer_form']//input[@name = 'phone']"));
     WebElement password = driver.findElement(By.xpath("//form[@name = 'customer_form']//input[@name = 'password']"));
     WebElement confirmed_password = driver.findElement(By.xpath("//form[@name = 'customer_form']//input[@name = 'confirmed_password']"));
-    WebElement submit = driver.findElement(By.xpath("//form[@name = 'customer_form']//button[@name = 'create_account']"));
-    new Actions(driver).moveToElement(nameField)
+
+    new Actions(driver).moveToElement(driver.findElement(By.xpath("//form[@name = 'customer_form']//input[@name = 'firstname']")))
             .click()
             .sendKeys("firstname_" + random + Keys.TAB + "lastname_" + random + Keys.TAB
             + "address1_" + random + Keys.TAB + Keys.TAB
             + index + Keys.TAB + "city_" + random)
-            .moveToElement(countriesList)
+            .moveToElement(driver.findElement(By.xpath("//form[@name = 'customer_form']//span[contains (@class, 'select2')]")))
             .click()
             .sendKeys("United States" + Keys.ENTER)
-            .moveToElement(zonesList)
+            .moveToElement(driver.findElement(By.xpath("//form[@name = 'customer_form']//input[@name = 'email']")))
             .click()
-            .moveByOffset(0, 10)
-            .click()
-            .moveToElement(email)
-            .click()
-            .sendKeys(random + "@mail.ru")
+            .sendKeys(email)
             .perform();
+
     phone.clear();
     new Actions(driver).moveToElement(phone)
             .click()
             .sendKeys(phoneNumber)
             .perform();
+
     password.clear();
     new Actions(driver).moveToElement(password)
             .click()
-            .sendKeys("password_" + random)
+            .sendKeys(pass)
             .perform();
+
     confirmed_password.clear();
     new Actions(driver).moveToElement(confirmed_password)
             .click()
-            .sendKeys("password_" + random)
+            .sendKeys(pass + Keys.ENTER)
             .perform();
 
-    submit.click();
-    // доделать логаут
-    // доделать повторную авторизацию
-    // доделать повторный логаут
-
+    driver.findElement(By.xpath("//div[@id = 'box-account']//a[contains(@href, 'logout')]")).click();
+    new Actions(driver).moveToElement(driver.findElement(By.xpath("//input[@name = 'email']")))
+            .click()
+            .sendKeys(email + Keys.TAB + pass + Keys.ENTER)
+            .perform();
+    driver.findElement(By.xpath("//div[@id = 'box-account']//a[contains(@href, 'logout')]")).click();
   }
 }
